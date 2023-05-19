24459a2b88d5c6131d2473c32488bcefa81f4d12
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-18 22:41:08 -0500

    removed unecessary imports

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,10 +1,9 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from "@apollo/client";
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
-import { LOGIN_USER } from "../utils/mutations"
+
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
