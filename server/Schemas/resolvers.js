afbba39aa9c344517c17200822b02e3942014a43
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-18 23:30:58 -0500

    fixed syntax

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -36,31 +36,29 @@ const resolvers = {
 
       return { token, user };
     },
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: input } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
   },
-},
-saveBook: async (parent, { input }, context) => {
-  if (context.user) {
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: context.user._id },
-      { $addToSet: { savedBooks: input } },
-      { new: true, runValidators: true }
-    );
-    return updatedUser;
-  }
-  throw new AuthenticationError("You need to be logged in!");
-},
-removeBook: async (parent, { bookId }, context) => {
-  if (context.user) {
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: context.user._id },
-      { $pull: { savedBooks: { bookId: bookId } } },
-      { new: true }
-    );
-    return updatedUser;
-  }
-  throw new AuthenticationError("You need to be logged in!");
-},
-},
 };
 
 module.exports = resolvers;
