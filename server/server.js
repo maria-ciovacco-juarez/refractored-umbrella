3c9061b9f802468a69f33d084db3eacc3e9496e8
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-19 12:41:09 -0500

    making corrections

--- server/server.js
+++ server/server.js
@@ -1,35 +1,50 @@
-const express = require("express");
-const path = require("path");
-const db = require("./config/connection");
+const express = require('express');
+const path = require('path');
 
-const { ApolloServer } = require("apollo-server-express");
-const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
+//import apollo server
+const { ApolloServer } = require('apollo-server-express');
+// import typeDefs and resolvers
+const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 
+//db connection
+const db = require('./config/connection');
+const routes = require('./routes');
+
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const startServer = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-  });
-  await server.start();
-  server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
-startServer();
+//apply apollo server with express ap
+server.applyMiddleware({app});
 
+//middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.use(routes);
+
+//get all
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}); 
+
