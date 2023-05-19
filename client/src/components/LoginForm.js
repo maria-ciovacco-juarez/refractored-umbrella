2600a58a8c0b3a4f24d7715d4350c2b5c4a574e7
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-18 22:39:15 -0500

    added to pages

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,9 +1,10 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { useMutation } from "@apollo/client";
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { LOGIN_USER } from "../utils/mutations"
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
